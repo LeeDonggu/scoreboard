ff57a42846a88fdf22f2d1a67420a08b7c768367
KMS06-24 <asd>
2019-03-02 12:05:17 +0900

    AddPlayerForm 로직 추가

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,16 +3,25 @@ import React from 'react';
 export class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isRunning: false
+    }
   }
+
+  handleStopwatch = () => {
+    this.setState(prevState => ({
+      isRunning: !prevState.isRunning
+    }));
+  };
 
   render() {
     return (
       <div className="stopwatch">
         <h2>Stopwatch</h2>
         <span className="stopwatch-time"></span>
-        <button>Start</button>
-        <button>Reset</button>
+        <button onClick={this.handleStopwatch}>
+          {this.state.isRunning ? 'Stop' : 'Start'}
+        </button>
       </div>
 
     );
